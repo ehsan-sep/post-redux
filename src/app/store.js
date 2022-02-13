3a3709cc35364eea88dd1088983e7d93ae6e5655
Arab Ahmadi <e.arabahmadi@sep.net>
2022-02-13 09:57:07 +0330

    senevthien

--- src/app/store.js
+++ src/app/store.js
@@ -1,11 +1,14 @@
-import { configureStore } from '@reduxjs/toolkit'
+import { configureStore, getDefaultMiddleware } from '@reduxjs/toolkit'
 import postsReducer from "../features/posts/postSlice"
 import usersReducer from "../features/users/userSlice"
 import notificationsReducer from "../features/notifications/notificationSlice"
+import {apiSlice} from "../features/api/apiSlice"
 export default configureStore({
   reducer: {
     posts:postsReducer,
     users:usersReducer,
-    notifications:notificationsReducer
-  }
+    notifications:notificationsReducer,
+    [apiSlice.reducerPath]:apiSlice.reducer
+  },
+  middleware: getDefaultMiddleware => getDefaultMiddleware().concat(apiSlice.middleware)
 })
